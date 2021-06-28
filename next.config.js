95ef8b64ce6d0c69b344c0e76147d07b71ae5420
Kenni G. Alamsyah <kgandira@gmail.com>
2021-06-28 22:34:31 +0700

    fix(fail-deployment): Update next config redirect

--- next.config.js
+++ next.config.js
@@ -4,16 +4,9 @@ module.exports = {
     },
     webpack(config) {
         config.module.rules.push({
-          test: /\.svg$/,
-          use: ["@svgr/webpack"]
+            test: /\.svg$/,
+            use: ['@svgr/webpack']
         });
         return config;
-    },
-    redirects() {
-        return [
-            process.env.MAINTENANCE_MODE === "1"
-            ? { source: "/((?!under-construction).*)", destination: "/under-construction", permanent: false }
-            : null,
-        ].filter(Boolean);
     }
 };
